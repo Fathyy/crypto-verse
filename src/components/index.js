d0e6ef77239664c69bdb1133b6f07740c2a3f2af
Fathi Abdi <fathiabdi254@gmail.com>
2024-05-06 16:16:25 +0300

    added the news api part

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from './Navbar';
+export { default as CryptoDetails } from './CryptoDetails';
+export { default as Loader } from './Loader';
+export { default as LineChart } from './LineChart';
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+    'X-RapidAPI-Host': process.env.DUCKDUCK_GO_HOST
+}
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://duckduckgo10.p.rapidapi.com/search'}),
+    endpoints: (builder) => ({
+      getCryptoNews: builder.query({
+        query: ({ newsCategory, count }) => createRequest(`?term=${newsCategory}&safeSearch=off&time=a&region=wt-wt&count=${count}`),
+      }),
+
+    }),
+  });
+  
+  export const { useGetCryptoNewsQuery } = cryptoNewsApi;
